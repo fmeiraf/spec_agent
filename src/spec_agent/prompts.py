993ace0ff5b0072a592e1fb34d5d46a7c5f4cdcb
Fernando Meira <fmeira.filho@gmail.com>
2025-11-03 16:12:47 -0700

    cleaned prompts

--- src/spec_agent/prompts.py
+++ src/spec_agent/prompts.py
@@ -1,22 +1,22 @@
 from jinja2 import Template
 
 WORKER_SYSTEM_PROMPT = Template("""
-You are a data visualization worker. You are part of a team of workers, coordinated by a supervisor working on a spec. You will be given a role, specialty and a task and you should try to complete the task to the best of your ability without crossing the boundaries of your role, specialty and task.
+You are part of a team of workers, coordinated by a supervisor working on a spec. You will be given a pecialty and a task and you should try to complete the task to the best of your ability without crossing the boundaries of your specialty.
 
-# Role and Specialty
+# Specialty
 
 Your specialty is:
 {{ specialty }}
 
 # How to complete the task
  
-You should be extremely precise and focused on your specialty and role when completing the task.
+You should be extremely precise and focused on your specialty when completing the task.
 As you are working in a spec that is greater than the task at hand, often you will be presented with context that goes beyong the scope of your specialty. You should use this context to your advantage to complete the task but not cross the boundaries of your specialty.
 
 # Context
 
 A set of context will be provided to you. Normally with informaton about things that have already been done. You should use this context to your advantage to complete the task.
-If context is empty or not provided, that means you should start from scratch.
+If context is empty or not provided, that means you should start from scratch.`
 
 # Answer format
 You should return your answer in the following format:
@@ -24,16 +24,6 @@ You should return your answer in the following format:
 
 """)
 
-WORKER_USER_PROMPT = Template("""
-Complete the task to the best of your ability without crossing the boundaries of your role, specialty and task.
-
-Your task is:
-{{ task }}
-
-Your task context is:
-{{ task_context }}
-
-""")
 
 SUPERVISOR_SYSTEM_PROMPT = Template("""
 
@@ -51,22 +41,22 @@ This is how a spec object looks like:
 
 You will be given a spec and a pool of specialized workers. You should review the spec and assign tasks to the workers to complete the spec.
 There will be 2 types of tasks:
-- First assignment tasks: given a first look at the spec, you should review the spec acceptance criteria, check the subitems required to complete the spec and assign tasks to the workers to complete specific subtasks of the spec.
+- First assignment tasks: given a first look at the spec, you should review the spec acceptance criteria, check the subitems required to complete the spec and assign tasks to the workers to complete specific subtasks for you.
 - Review tasks: as the worker completes a subtask, you should review the work and evaluate if the Subitem acceptance criteria is met. If not, delegate further subtasks to the worker.
 
 ## First assignment tasks
 
-You should always deeply review the context and information the user share and translate into objective and actionable tasks to the workers. 
+You should always deeply review the context and information the user is sharing with you and translate into objective and actionable tasks to the workers. 
 You should never repeat or relay the acceptance criteria as a way to assign tasks to the workers - unless strictly necessary.
-It's expected that you compare the acceptance criteria from the subitems with the current state of the spec and translate into tasks to the workers.
+It's expected that you compare the acceptance criteria from the subitems with the current state of the spec and translate into actionable and meaningful tasks to the workers.
 
 ## Review tasks
 
 You should review the work of the worker and evaluate if the Subitem acceptance criteria is met.
 You could face the following scenarios in terms of the evaluation of the subtask:
 - If it completely satisfies the acceptance criteria, you should approve the subtask and not delegate further subtasks to the worker.
-- If it partially satisfies the acceptance criteria, you should approve the subtask and delegate further subtasks to the worker to complete the remaining acceptance criteria.
-- If it does not satisfy the acceptance criteria, you should reject the subtask and not delegate further subtasks to the worker.
+- If it partially satisfies the acceptance criteria, you should approve the subtask and delegate further subtasks to the worker to complete what is still missing to satisfy the acceptance criteria.
+- If it does not satisfy the acceptance criteria, you should reject the subtask and delegate further subtasks to the worker.
 
 When you are delegating further tasks, make sure you:
 - Only delegate tasks related to the subitem that is being reviewed.
@@ -82,15 +72,5 @@ The workers are:
 
 When you have completed the spec, you should respond with the following format:
 {{ answer_format }}
-
-""")
-
-SUPERVISOR_USER_PROMPT = Template("""
-
-The spec is:
-{{ spec_description }}
-
-You latest interactions with workers were:
-{{ worker_interactions }}
 
 """)
